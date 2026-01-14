f76feaf1cf088138bc4ce833bea640075e0a9d10
Jayant71 <110401230+Jayant71@users.noreply.github.com>
2026-01-14 16:04:39 +0530

    add process time middleware

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
-
-from app.routers import cart, orders
+from app import models
+from app.core.middleware import add_process_time_header
+from app.routers import cart, category, orders
 from .routers import products, auth
 from dotenv import load_dotenv
-from .database import engine, Base
+from .database import engine
 
 load_dotenv()
-Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(
@@ -21,6 +22,15 @@ app.include_router(
 app.include_router(
     orders.router
 )
+
+app.include_router(
+    category.router
+)
+
+
+@app.middleware("http")
+async def process_time_middleware(request, call_next):
+    return await add_process_time_header(request, call_next)
 
 
 @app.get("/")
